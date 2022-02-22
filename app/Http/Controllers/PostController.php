c7819afc4d4605efb586a603fd86a03580554bcf
shaimaa <shimaaashraf1011@gmail.com>
2022-02-23 00:29:59 +0200

    first commit

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -16,6 +16,7 @@ class PostController extends Controller
     public function index()
     {
         $posts=Post::all();
+        // $post->user;
         return view("posts.index",["posts"=>$posts]);
 
     }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -11,8 +11,10 @@ class Post extends Model
 
 
    protected $fillable  = ["title", "description"];
-       protected $hidden = [
-        'created_at', 'updated_at',
-    ];
-
+    //    protected $hidden = [
+    //     'created_at', 'updated_at',
+    // ];
+public function user(){
+return $this->belongsTo(User::class);
+}
 }
